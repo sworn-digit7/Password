828cbda99669d5ac5c99485dac4360e2ce16206a
Gurleen Singh <singhgurleen551@gmail.com>
2026-09-01 18:47:53 +0100

    adding to file added

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
+import csv
+
 def main():
-    site = input("What is the name of the site? ")
-    username = input("What is your username? ")
-    password = input("What is your chosen password? ")
-    add_entry(site, username, password)
+    option = int(input("""Please choose one option:
+1. Add new password 
+2. Retrieve a password
+3. Delete a password
+4. See all passwords
+5. Create a strong password
+Which option would you like to choose:  """))
+
+    if option == 1:
+
+        site = input("What is the name of the site? ")
+        username = input("What is your username? ")
+        password = input("What is your chosen password? ")
+        add_entry(site, username, password)
+        print("password added!")
+
+    elif option == 2:
+        get_entry()
+
+    elif option == 3:
+        delete_entry()
+
+    elif option == 4:
+        list_entries()
+
+    elif option == 5:
+        generate_password()
+    
 
 def add_entry(site, username, password):
-    passwords = {}
-
-    passwords["site: "] = site
-    passwords["username: "] = username
-    passwords["password: "] = password
-    print(passwords)
+    with open("passwords.csv", "a") as file:
+        fieldnames = ["site", "username", "password"]
+        writer = csv.DictWriter(file, fieldnames = fieldnames)
+        writer.writeheader()
+        writer.writerow({'site': site, 'username': username, 'password': password})
 
 def get_entry(site):
     ...
